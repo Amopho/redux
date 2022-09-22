215f51a636be5b3c7a2482cdd33a9f72e3e2ee4a
amopho <amopho78@gmail.com>
2022-09-22 19:01:53 +0200

    currying

---
+++ src/functional/currying.js
@@ -0,0 +1,5 @@
+// currying is a function that allow us to get an N argumets and create a function with a single argument
+//N=> 1
+
+//the clue is to seprate arguments not with a comma (a,b) but with paranthesis brackets or with two arrows
+// in a arrow function
--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ let input = "    Javascript    ";
 //  wrap into div
 // lowercsase
 const trim = str => str.trim();
-const wrap = (str, type) =>  `<${type}>${str}</${type}>`;
+// applying a currying function, seperating arguments with arrows instead of comma
+// const wrap = (str, type) =>  `<${type}>${str}</${type}>`;
+const wrap = type=> str =>  `<${type}>${str}</${type}>`;
+
 const toLowerCase = str => str.toLowerCase();
 // ugly code tranformed with pipe 
 // const result = wrapInDiv(toLowerCase(trim(input)));
@@ -20,7 +23,8 @@ const toLowerCase = str => str.toLowerCase();
 // pipe builds a pipeline the output of each function ends up of being an output of the next one, in this case the second pipeline
 // output is a lowercase string, the second parameter is empty, and that's why the console log says undefined, by giving the div as 
 //a type element to the wrap we get another error "expected a function", it produces a streing, but every argument given to the pipe has to be function
-// we need a funtion with a single parameter instead of two parametres - currying solves this problem
+// we need a funtion with a single parameter instead of two parametres - currying solves this problem. 
+//After applying currying to wrap("div we get a funtion instead of a string"). Now we can pass this function into a pipe.
 const transform = pipe(trim, toLowerCase, wrap("div"));
 console.log(transform(input));
 
